6cdfc3bddb3a7d6a106431a7d883bb8c6de19249
meilannptr13 <114566692+meilannptr13@users.noreply.github.com>
2023-03-21 18:43:52 +0700

    week3

---
+++ tamplate/content.php
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+<div id="layoutSidenav_content">
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">Dashboard</h1>
+            <p>Hello World ini adalah Dashboard
+                
+            </p>
+        </div>
+    </main>
+=======
+<div id="layoutSidenav_content">
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">Dashboard</h1>
+            <p>Hello World ini adalah Dashboard
+                
+            </p>
+        </div>
+    </main>
+>>>>>>> cf48333 (upload week4)
+</div>        
---
+++ tamplate/index.php
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+<!-- panggil file atas.php -->
+<?php
+require_once('atas.php')
+?>
+<!-- panggil file bawah.php -->
+<?php
+require_once('content.php
+')
+?>
+<!-- panggil file bawah.php -->
+<?php
+require_once('bawah.php')
+=======
+<!-- panggil file atas.php -->
+<?php
+require_once('atas.php')
+?>
+<!-- panggil file bawah.php -->
+<?php
+require_once('content.php
+')
+?>
+<!-- panggil file bawah.php -->
+<?php
+require_once('bawah.php')
+>>>>>>> cf48333 (upload week4)
+?>
